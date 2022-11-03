5c4dadaca2ff6b4a34ed441415e0827ae9f50ae7
Juan Duzac <56564649+JDuzy@users.noreply.github.com>
2022-11-03 15:11:48 -0300

    Finished MVP of MovieListScreen (#4)
    added colors and personalized theme

--- app/src/main/java/com/juanduzac/movieapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/juanduzac/movieapp/presentation/ui/theme/Theme.kt
@@ -1,15 +1,18 @@
 package com.juanduzac.movieapp.presentation.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = LightBlue,
+    primaryVariant = DarkBlue,
+    secondary = LightBlue,
+    background = DarkGrey,
+    surface = Black,
+    onBackground = LightGrey,
+    onSurface = White
 )
 
 private val LightColorPalette = lightColors(
@@ -28,7 +31,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun MovieAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun MovieAppTheme(darkTheme: Boolean = true, content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
